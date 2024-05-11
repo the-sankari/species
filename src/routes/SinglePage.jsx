d62550fec0db3bf14cd67a736905ff24d3e24dee
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-05-11 13:32:42 +0300

    Files updated

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -5,8 +5,33 @@ function SinglePage({ ...rest }) {
   const categoryItem = rest[params.category];
   const data = categoryItem.find((el) => el.name === params.name);
   return (
-    <div className="single">
-      <h3> {data.name} </h3>
+    <div className="container singlePage">
+      <div className="single">
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src={`https://source.unsplash.com/400x400?${data.name}`}
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title"> {data.name} </h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
